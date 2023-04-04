4475cb6cfdb048e9add69d57384b725d6b4668f8
Amy Terry-Penak <amy@bendyworks.com>
2023-04-04 16:34:08 -0500

    customize the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -6,7 +7,10 @@ import Seo from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>The page you were looking for doesn't exist.</p>
+    <p>
+      <Link to="/">Go back home</Link>
+    </p>
   </Layout>
 )
 
