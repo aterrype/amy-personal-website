d9ad49910ad55de8aa31a25b5aeddbbc8c37a396
Amy Terry-Penak <amy@bendyworks.com>
2023-04-17 15:51:34 -0500

    functional programming blog post

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,19 @@ module.exports = {
         path: `${__dirname}/src/content`,
       },
     },
-    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 800,
+            },
+          },
+        ],
+      },
+    },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
